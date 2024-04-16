a6c65ef806cf8f9614e6e1501363535099a05fd8
EC2 Default User <ec2-user@ip-172-31-7-104.ap-south-1.compute.internal>
2024-04-16 07:01:25 +0000

    Updates..

--- Reddit_image_scraper.py
+++ Reddit_image_scraper.py
@@ -2,6 +2,8 @@ import os
 import praw
 import configparser
 import urllib.request
+from tqdm import tqdm
+from time import sleep
 
 from prawcore.exceptions import Redirect
 from prawcore.exceptions import ResponseException
@@ -43,7 +45,10 @@ def delete_img_list():
 def get_img_urls(sub, li):
     try:
         r = praw.Reddit(client_id=ClientInfo.id, client_secret=ClientInfo.secret, user_agent=ClientInfo.user_agent)
-        submissions = r.subreddit(sub).hot(limit=li*5)
+        if hot:
+            submissions = r.subreddit(sub).hot(limit=li*5)
+        else:
+            submissions = r.subreddit(sub).top(time_filter="all", limit=li*5)
 
         return [submission.url for submission in submissions]
 
@@ -59,23 +64,31 @@ def get_img_urls(sub, li):
         print("Client info is wrong. Check again.")
         return 0
 
+    except Exception as e:
+        print("Unexpected Error:", e)
+        return 0
+
 
 def download_img(img_url, img_title, filename):
     opener = urllib.request.build_opener()
     opener.addheaders = [('User-agent', 'Mozilla/5.0')]
     urllib.request.install_opener(opener)
     try:
-        print('Downloading ' + img_title + '....')
+        # print('Downloading ' + img_title + '....')
         urllib.request.urlretrieve(img_url, filename)
         return 1
 
-    except HTTPError:
-        print("Too many Requests. Try again later!")
+    except HTTPError as e:
+        print("Too many Requests. Try again later!, ", e)
         return 0
-        
-    except OSError:
-        print(OSError)
-        return 0 
+
+    except OSError as e:
+        print("OSError:", e)
+        return 0
+
+    except Exception as e:
+        print("Unexpected Error:", e)
+        return 0
 
 def read_img_links(sub, limit, tolerance=3):
     failed = 0
@@ -85,9 +98,12 @@ def read_img_links(sub, limit, tolerance=3):
     links = [x.strip() for x in links]
     download_count = 0
 
-    for link in links:
+    for link in tqdm(links, total=limit):
         if not is_img_link(link):
             continue
+
+        if(download_count == limit):
+            return download_count, 1
 
         file_name = link.split('/')[-1]
         file_loc = 'result/{}/{}'.format(sub, file_name)
@@ -100,17 +116,15 @@ def read_img_links(sub, limit, tolerance=3):
             continue
 
         download_status = download_img(link, file_name, file_loc)
-        download_count += 1
-
-        if(download_count == limit):
-            return download_count, 1
+        sleep(3)
 
         if download_status == 0:
             failed+=1
             if(failed==tolerance):
                 return download_count, 0
-                
             continue
+        else:
+            download_count += 1
 
     return download_count, 1
 
@@ -120,16 +134,17 @@ if __name__ == '__main__':
     ClientInfo.id, ClientInfo.secret = get_client_info()
 
     subreddit = input('Enter Subreddit: ')
-    num = int(input('Enter Limit: '))
-    url_list = get_img_urls(subreddit, num)
+    hot = bool(input('0 For Top, 1 For Hot: '))
+    limit = int(input('Enter Limit: '))
+    url_list = get_img_urls(subreddit, limit)
 
     if url_list:
 
         save_list(url_list)
-        count, status = read_img_links(subreddit, num)
+        count, status = read_img_links(subreddit, limit)
 
         if status == 1:
-            print('\nDownload Complete\n{} - Images Downloaded\n{} - Posts Ignored'.format(count, num - count))
+            print(f'\nDownload Complete\n{count} - Images Downloaded.')
         elif status == 0:
             print('\nDownload Incomplete\n{} - Images Downloaded'.format(count))
 
